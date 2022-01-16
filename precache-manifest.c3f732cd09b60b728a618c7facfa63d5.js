ebdc0d02ae172a7eafbaaa8ccbd3f63f7e311e2e
urstrulykoustubh <70585076+urstrulykoustubh@users.noreply.github.com>
2022-01-16 11:55:47 +0530

    Updates

---
+++ precache-manifest.c3f732cd09b60b728a618c7facfa63d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77334cce232eef1a445a",
+    "url": "/me/static/css/main.1527b0a5.chunk.css"
+  },
+  {
+    "revision": "77334cce232eef1a445a",
+    "url": "/me/static/js/main.77334cce.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "7839c8f270bf379113eab6bf5bc2e2a1",
+    "url": "/me/index.html"
+  }
+];
